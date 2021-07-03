35cc2572801985bafaabf6f80a9a06987a36e248
qassandrach <qassiechaidir@gmail.com>
2021-07-03 22:00:43 +0700

    Add first API try

---
+++ bmi.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+@app.route("/")
+def index():
+
+    weight = request.args.get('weight', type=int)
+    height = request.args.get('height', type=int)
+
+    bmi = round((weight/pow((height/100),2)),2)
+    health_status = ""
+
+    if bmi < 18.5:
+        health_status = "underweight :("
+    elif bmi > 18.5 or bmi <= 24.9:
+        health_status = "healthy :)"
+    elif bmi >= 25.0:
+        health_status = "overweight :("
+    
+    return {"bmi": bmi,
+    "label": health_status}
