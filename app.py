ed10cff50b8a8a97df2c43f55d1594e8eb4430ff
qassandrach <qassiechaidir@gmail.com>
2021-07-05 02:49:58 +0700

    Fix routing

--- app.py
+++ app.py
@@ -1,28 +1,33 @@
 from flask import Flask, request
-from flask_restful import Api
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
 
-@app.route("/")
-def index():
+# @app.route("/")
+class Bmi (Resource):
 
-    weight = request.args.get('weight', type=int)
-    height = request.args.get('height', type=int)
+    def get(self):
 
-    bmi = round((weight/pow((height/100),2)),2)
-    health_status = ""
+        weight = request.args.get('weight', type=int)
+        height = request.args.get('height', type=int)
 
-    if bmi < 18.5:
-        health_status = "underweight :("
-    elif bmi >= 18.5 and bmi <= 24.9:
-        health_status = "healthy :)"
-    elif bmi >= 25.0:
-        health_status = "overweight :("
-    
-    return {"bmi": bmi,
-    "label": health_status}
+        bmi = round((weight/pow((height/100),2)),2)
+        health_status = ""
+
+        if bmi < 18.5:
+            health_status = "underweight :("
+        elif bmi >= 18.5 and bmi <= 24.9:
+            health_status = "healthy :)"
+        elif bmi >= 25.0:
+            health_status = "overweight :("
+        
+        return {"bmi": bmi,
+        "label": health_status}
+
+api.add_resource(Bmi, '/')
+
 
 if __name__ == '__main__':
    app.run('0.0.0.0','5000')
