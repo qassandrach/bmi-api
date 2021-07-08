332fe5a8f6bfd1fc30f5608a258ab488b37d0c86
qassandrach <qassiechaidir@gmail.com>
2021-07-08 17:04:00 +0700

    add gitignore

--- app/request.py
+++ app/request.py
@@ -3,7 +3,10 @@ from flask import request
 from bmi import BmiStatus
 
 class Request(Resource):
+
+
     def get(self):
+        
         weight = request.args.get('weight', default=None, type=int)
         height = request.args.get('height', default=None, type=int)
         
--- app/test_app.py
+++ app/test_app.py
@@ -3,14 +3,18 @@ import json, unittest
 import app
 
 class TestCases(unittest.TestCase):
+
+
     def setUp(self):
         app.app.config['TESTING'] = True
         self.app = app.app.test_client()
+
 
     def test_without_args(self):
         response = self.app.get("/")
 
         self.assertEqual(response.status_code, 200)
+
 
     def test_with_args(self):
         response = self.app.get("/?weight=50&height=167")
@@ -20,11 +24,13 @@ class TestCases(unittest.TestCase):
         self.assertEqual(data["label"], "underweight")
         self.assertEqual(response.status_code, 200)
 
+
     def test_invalid_value(self):
         response = self.app.get("/?weight=0&height=0")
         data = json.loads(response.get_data(as_text=True))
 
         self.assertEqual(data["error"], "float division by zero")
+        
 
 unittest.main()
 
